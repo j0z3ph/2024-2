6326a1f6fe5eb9f2ae057f54f88c5a8aecb3b12e
Jose Luis Cruz <j0z3ph@gmail.com>
2024-03-19 10:55:09 -0600

    sepa

--- ADS 1TV1/p5/inversa.cpp
+++ ADS 1TV1/p5/inversa.cpp
@@ -14,19 +14,28 @@ using namespace std;
 
 int main()
 {
-    int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int matrixT[3][3];
+    int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {6, 8, 9}};
+    int matrixT[3][3], matrixA[3][3];
+    int determinante;
+    float factor, matrixI[3][3];
 
-    for (int f = 0; f < 3; f++)
+    // Determinante
+    determinante = matrix[0][0] * matrix[1][1] * matrix[2][2] +
+                   matrix[0][1] * matrix[1][2] * matrix[2][0] +
+                   matrix[0][2] * matrix[1][0] * matrix[2][1] -
+                   matrix[2][0] * matrix[1][1] * matrix[0][2] -
+                   matrix[2][1] * matrix[1][2] * matrix[0][0] -
+                   matrix[2][2] * matrix[1][0] * matrix[0][1];
+
+    if (determinante == 0)
     {
-        for (int c = 0; c < 3; c++)
-        {
-            cout << matrix[f][c] << " ";
-        }
-        cout << endl;
+        cout << "Upss, no tiene inversa :'(" << endl;
+        return 1;
     }
 
-    cout << endl;
+    factor = 1.0f / (float)determinante;
+
+    // Traspuesta
 
     for (int f = 0; f < 3; f++)
     {
@@ -34,19 +43,38 @@ int main()
         {
             matrixT[f][c] = matrix[c][f];
         }
-        
+    }
+
+    // Adjunta
+    matrixA[0][0] = +1 * (matrixT[1][1] * matrixT[2][2] - matrixT[2][1] * matrixT[1][2]);
+    matrixA[0][1] = -1 * (matrixT[1][0] * matrixT[2][2] - matrixT[1][2] * matrixT[2][0]);
+    matrixA[0][2] = +1 * (matrixT[1][0] * matrixT[2][1] - matrixT[2][0] * matrixT[1][1]);
+    matrixA[1][0] = -1 * (matrixT[0][1] * matrixT[2][2] - matrixT[0][2] * matrixT[2][1]);
+    matrixA[1][1] = +1 * (matrixT[0][0] * matrixT[2][2] - matrixT[0][2] * matrixT[2][0]);
+    matrixA[1][2] = -1 * (matrixT[0][0] * matrixT[2][1] - matrixT[0][1] * matrixT[2][0]);
+    matrixA[2][0] = +1 * (matrixT[0][1] * matrixT[1][2] - matrixT[1][1] * matrixT[0][2]);
+    matrixA[2][1] = -1 * (matrixT[0][0] * matrixT[1][2] - matrixT[0][2] * matrixT[1][0]);
+    matrixA[2][2] = +1 * (matrixT[0][0] * matrixT[1][1] - matrixT[0][1] * matrixT[1][0]);
+
+    // Multiplicamos
+    for (int f = 0; f < 3; f++)
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            matrixI[f][c] = factor * (float)matrixA[f][c];
+        }
     }
 
     for (int f = 0; f < 3; f++)
     {
         for (int c = 0; c < 3; c++)
         {
-            cout << matrixT[f][c] << " ";
+            cout << matrixI[f][c] << " ";
         }
         cout << endl;
     }
-    
-    
+
+    cout << endl;
 
     return 0;
 }

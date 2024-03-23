5f04ab841134337c143660f6cb6d3a57380e2493
Jose Luis Cruz <j0z3ph@gmail.com>
2024-03-22 18:59:15 -0600

    Ejemplo de encapsulamiento

---
+++ ADS 1TV1/P10/tools.hpp
@@ -0,0 +1,53 @@
+/**
+ * @file tools.h
+ * @author Jose Luis Cruz (j0z3ph@gmail.com)
+ * @brief Tools to make interactive menus.
+ * @version 0.1
+ * @date 2022-11-30
+ */
+
+#ifndef __TOOLS_H__
+#define __TOOLS_H__
+#include<iostream>
+
+using namespace std;
+
+/**
+ * @brief Limpia el buffer despues de un cin
+ * 
+ */
+void limpiarBuffer();
+
+/**
+ * @brief Pausa la ejecucion del programa
+ * 
+ */
+void pausa();
+
+/**
+ * @brief Moves cursor to desired coordinates.
+ * 
+ * @param x X coordinate (column).
+ * @param y Y coordinate (row).
+ */
+void gotoXY(int x, int y);
+
+/**
+ * @brief Clears command window.
+ * 
+ */
+void clear();
+
+/**
+ * @brief Prints the given menu. 
+ * User can interact with menu using Up/Down arrows
+ * and can select an option pressing the ENTER key.
+ * 
+ * @param numItems Number of items in the string array.
+ * @param items String array. Each item is a menu option.
+ * @param title Menu title.
+ * @return int 0-base index of the selected item.
+ */
+int showMenu(int numItems, string items[], const string title);
+
+#endif

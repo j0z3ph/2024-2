f0d4d1e88f6e527f468309fba5fdeba8e16b997f
Jose Luis Cruz <j0z3ph@gmail.com>
2024-03-25 11:08:16 -0600

    Added license

--- IP 1MM2/P14/tools.h
+++ IP 1MM2/P14/tools.h
@@ -4,6 +4,18 @@
  * @brief Tools to make interactive menus.
  * @version 0.1
  * @date 2022-11-30
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __TOOLS_H__

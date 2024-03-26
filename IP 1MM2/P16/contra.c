d16faf63f64fb3aa019b7207ef59ccc46e98235a
Jose Luis Cruz <j0z3ph@gmail.com>
2024-03-26 14:38:10 -0600

    Ejemplos de recursion

---
+++ IP 1MM2/P16/contra.c
@@ -0,0 +1,17 @@
+/**
+ * @file contra.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief 
+ * @version 0.1
+ * @date 2024-03-26
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
